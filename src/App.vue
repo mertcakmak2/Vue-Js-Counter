5eb75a0a94cd4149c3939a19bc6f7847e3cf4cd4
mertcakmak <mertcakmak2@gmail.com>
2020-09-08 23:49:01 +0300

    vuex commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Vuex</h1>
+                <app-result></app-result>
+                <app-another-result></app-another-result>
+                <hr>
+                <app-counter></app-counter>
+                <app-another-counter></app-another-counter>
+                <hr>
+                <input type="text" v-model="value">
+                <p>{{ value }}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Counter from './components/Counter.vue';
+    import AnotherCounter from './components/AnotherCounter.vue';
+    import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        computed: {
+          value: {
+              get() {
+                  return this.$store.getters.value;
+              },
+              set(value) {
+                  this.$store.dispatch('updateValue', value);
+              }
+          }
+        },
+        components: {
+            appCounter: Counter,
+            appAnotherCounter: AnotherCounter,
+            appResult: Result,
+            appAnotherResult: AnotherResult,
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>Counter is: {{ counter }}</p>
+</template>
+
+<script>
+    // import {mapGetters} from 'vuex';
+    export default {
+        computed: {
+            counter(){
+                return this.$store.getters.doubleCounter
+            }
+        }
+    }
+</script>
